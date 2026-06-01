3a87829e3b7401a1db535c11f6992f538c900717
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-06-02 02:23:34 +0530

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.lastIndex
+
+
+        while (l <= r) {
+            val m = l + (r-l)/2
+
+            if(target == nums[m]) return m
+
+            if(nums[l] <= nums[m]) {
+                if(target > nums[m] || target < nums[l]) {
+                    l = m + 1
+                } else {
+                    r = m -1
+                }
+            } else {
+                if(target < nums[m] || target > nums[r]) {
+                    r = m - 1
+                } else {
+                    l = m + 1
+                }
+            }
+        }
+        return -1
+    }
+}
