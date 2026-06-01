f90dd93134f78985cd2140c84618cd52af910213
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-06-01 13:43:22 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.kt
@@ -0,0 +1,31 @@
+class Solution {
+
+    fun encode(strs: List<String>): String {
+        return strs.map {
+            it.length.toString() + "#" + it
+        }.joinToString("")
+    }
+
+    fun decode(str: String): List<String> {
+        val res =  mutableListOf<String>()
+
+        var i = 0 
+
+        while(i < str.length) {
+            var j = i
+
+            while (str[j] != '#') {
+                j++
+            }
+
+            val length = str.substring(i, j).toInt()
+
+            i = j + 1
+
+            res.add(str.substring(i, i + length))
+
+            i = i + length
+        }
+        return res
+    }
+}
