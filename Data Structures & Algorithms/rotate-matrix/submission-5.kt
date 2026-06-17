e88045dfa02f2359a7aa919a4a9b8ca63571bff5
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-06-17 08:05:04 +0530

    Add: rotate-matrix - submission-5

---
+++ Data Structures & Algorithms/rotate-matrix/submission-5.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun rotate(matrix: Array<IntArray>) {
+        var left = 0
+        var right = matrix.lastIndex
+
+        while(left <= right) {
+            for(i in 0 until right-left) {
+                val top = left
+                val bottom = right
+
+                val temp = matrix[top][left+i]
+                matrix[top][left+i] = matrix[bottom-i][left]
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                matrix[bottom][right-i] = matrix[top+i][right]
+                matrix[top+i][right] = temp
+            }
+            right--
+            left++
+        }
+    }
+}
