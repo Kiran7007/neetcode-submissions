366d825c55601f69afb674977e769f97792c0c56
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-07-20 09:37:39 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun change(amount: Int, coins: IntArray): Int {
+        val dp = IntArray(amount + 1)
+        dp[0] = 1
+
+        for(c in coins) {
+            for(a in 1 .. amount) {
+                if(a - c >= 0) {
+                    dp[a] += dp[a - c] 
+                }
+            }
+        }
+
+        return dp[amount]
+    }
+}
