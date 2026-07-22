5b5f068af05f9c42f80064734f97d5161cf3bd8c
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-07-22 09:36:59 +0530

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun numDistinct(s: String, t: String): Int {
+        val m = s.length
+        val n = t.length
+        val dp = IntArray(n+1)
+
+        dp[n] = 1
+
+        for(i in m - 1 downTo 0) {
+            var prev = 1
+            for(j in n - 1 downTo 0) {
+                var res = dp[j]
+                if(s[i] == t[j]) {
+                    res += prev
+                }
+                prev = dp[j]
+                dp[j] = res
+            }
+        }
+
+        return dp[0]
+    }
+}
