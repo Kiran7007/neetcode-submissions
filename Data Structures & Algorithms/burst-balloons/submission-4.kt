b70273d826239df2710bd505a257170659d48e6b
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-07-23 10:15:33 +0530

    Add: burst-balloons - submission-4

---
+++ Data Structures & Algorithms/burst-balloons/submission-4.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun maxCoins(nums: IntArray): Int {
+        val n = nums.size
+        val newNums = intArrayOf(1) + nums + intArrayOf(1)
+
+        val dp = Array(n+2) { IntArray(n+2) }
+
+        for(l in n downTo 1) {
+            for ( r in l .. n) {
+                for(i in l .. r) {
+                    val coins = newNums[l-1] * newNums[i] * newNums[r+1]
+                    dp[l][r] = maxOf(dp[l][r], coins + dp[l][i-1] + dp[i+1][r])
+                }
+            }
+        }
+
+        return dp[1][n]
+    }
+}
