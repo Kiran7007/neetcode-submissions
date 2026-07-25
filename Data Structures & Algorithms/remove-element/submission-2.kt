7e22e4d6c116559ef7cf87839cc250a01603eb1c
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-07-25 18:13:29 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var i = 0
+        var n = nums.size
+
+        while(i < n) {
+            if(nums[i] == `val`) {
+                n--
+                nums[i] = nums[n]
+            } else {
+                i++
+            }
+        }
+
+        return n
+    }
+}
