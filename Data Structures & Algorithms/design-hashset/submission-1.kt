8559e7e82198b0f23cc4ab357c4278b02420f925
Kiran Punwatkar <kiran.punwatkar@gmail.com>
2026-07-25 19:18:44 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.kt
@@ -0,0 +1,24 @@
+class MyHashSet() {
+
+    val arr = BooleanArray(1000001)
+
+    fun add(key: Int) {
+        arr[key] = true
+    }
+
+    fun remove(key: Int) {
+        arr[key] = false
+    }
+
+    fun contains(key: Int): Boolean {
+        return arr[key]
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
